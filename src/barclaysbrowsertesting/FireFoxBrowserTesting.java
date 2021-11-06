d3a974be3bf631a18a42eceded317ec6a2070692
yellowsunshine <sidnav999@gmail.com>
2021-11-06 21:08:07 +0000

    My homework week 10 Barclays

---
+++ src/barclaysbrowsertesting/FireFoxBrowserTesting.java
@@ -0,0 +1,30 @@
+package barclaysbrowsertesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxBrowserTesting {
+
+    public static void main(String[] args) {
+        String baseUrl = "https://www.barclays.co.uk/";
+
+        //Setting the driver path
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+
+        //Creating an object of the web driver
+        WebDriver driver = new FirefoxDriver();
+
+        //Selenium method used to launch the driver
+        driver.get(baseUrl);
+
+        //maximising the browser window
+        driver.manage().window().maximize();
+
+        //printing the browser title in the console
+        String name = driver.getTitle();
+        System.out.println(name);
+
+        //closing the browser window
+        driver.close();
+    }
+}
